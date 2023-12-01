8d51d1a2f074c076789becd2a2b4a47fb2ee85f1
kimemia21 <bobbymbogo71@gmail.com>
2023-12-01 15:31:05 +0300

    PHASE 0

---
+++ src/components/About.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+function About(props) {
+    return (
+        <div className='about-middle'>
+            <div className='about-left'>
+                <h1>ABOUT ME</h1>
+                <p>Kimemia is a juniour  software developer Based in Kenya</p>
+                <img src='https://static.vecteezy.com/system/resources/previews/022/665/512/original/enthusiastic-3d-student-boy-with-thumbs-up-on-white-background-transparent-background-free-png.png'  />
+
+
+            </div>
+            <div className='about-right'>
+                <h1>I,m Kimemia Thuku</h1>
+                <h4>A Lead <strong>Fullstack</strong> software engineer based in <strong>Kenya</strong>.</h4>
+
+                <p>
+                I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores.
+                 My passion is to design digital user experiences through the bold interface and meaningful interactions.
+                </p>
+
+                <div className='about-left-details'>
+                   <div className='left-details'>
+                    <span>Birthday</span>
+                    <p>4th April 2001</p>
+                   </div>
+
+                   <div className='left-details'>
+                    <span>E-mail</span>
+                    <p>bobbymbogo71@gmail.com</p>
+                   </div>
+
+                   
+
+                
+                </div>
+
+
+                <div className='about-left-details'>
+                   <div className='left-details'>
+                    <span>Age</span>
+                    <p>22 Yr</p>
+                   </div>
+
+                   <div className='left-details'>
+                    <span>Phone</span>
+                    <p>+254-769-922-984</p>
+                   </div>
+
+                   
+
+                
+                </div>
+
+
+
+                <div className='about-left-details'></div>
+                <div className='about-left-details'></div>
+                <div className='about-left-details'></div>
+
+              
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Top.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import Typed from "typed.js"
+import "../App.css"
+function Top() {
+
+   
+    useEffect(()=>{
+        const typedElement = document.querySelector('.typed');
+        if(typedElement){
+            new Typed(".typed",{
+    
+                strings:['Developer','Innovator','Designer','Artist'],
+                typeSpeed:100,
+                backSpeed: 100,       // Adjust this value for backspacing speed
+                backDelay: 1000,     // Delay before starting to backspace
+                startDelay: 50,  
+                loop:true,
+              
+            })
+        }
+
+    },[])
+
+ 
+
+   
+
+
+    
+    return (
+        <div className='top'>
+        <div className='info-top'>          
+          <h3>Hello there...</h3>
+          <h1 >Kimemia Thuku </h1>
+          <p  className='p-typed'> I Am Passionate  <span className="typed"></span></p>
+          <p>As a junior software developer, I am passionate about crafting efficient and elegant solutions to complex problems, continuously expanding my skills to contribute meaningfully to innovative projects.</p>
+          
+
+          <div className='top-links'>
+          <a href=''>
+            <button className='btn'>About me </button>
+            </a>
+        
+          
+            <a href=''>
+            <button className='btn'>My Work</button>
+            </a>
+          
+          </div>
+          
+          
+          </div>
+          <div className='img-top'>
+            <img src='https://static.vecteezy.com/system/resources/previews/022/484/140/non_2x/qualified-3d-electrician-with-cable-splicer-great-for-electrical-or-industrial-designs-transparent-background-free-png.png'/>
+          </div>
+          
+
+          </div>
+          )
+
+
+     
+}
+
+export default Top;
